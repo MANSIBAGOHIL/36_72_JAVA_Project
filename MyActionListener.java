88d8034ea51cd76554d8defceaa6c1075afd5619
MANSIBAGOHIL <92724158+MANSIBAGOHIL@users.noreply.github.com>
2022-05-22 13:32:04 +0530

    Update MyActionListener.java

--- MyActionListener.java
+++ MyActionListener.java
@@ -1,18 +1,30 @@
 //package MovieTicket;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.*;
+import java.awt.event.*;
+//import java.io.*;
+//import javax.swing.*;
+//import java.lang.*;
 
 public class MyActionListener implements ActionListener
 {
 	MyFrame mf;
+	MyFrame1 mf1;
 	
 	MyActionListener(MyFrame m){
 		this.mf = m;
 	}
+	MyActionListener(MyFrame1 m){
+		this.mf1 = m;
+	}
 	
 	public void actionPerformed(ActionEvent e)
 	{
-		System.out.println(e.getActionCommand());
-	}
+		if(e.getActionCommand().equals("Theatre name"))
+		{
+			mf1 = new MyFrame1();
+			this.mf1.setVisible(true);
+			this.mf.setVisible(false);
+		}
+	}	
 }
