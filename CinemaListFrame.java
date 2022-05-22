87167a26d1c3f688fbd1e96243a2dbfeeacbee4a
MANSIBAGOHIL <92724158+MANSIBAGOHIL@users.noreply.github.com>
2022-05-22 17:43:57 +0530

    added changes

--- CinemaListFrame.java
+++ CinemaListFrame.java
@@ -2,32 +2,59 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.ImageIcon;
 
-public class CinemaListFrame extends Frame
+public class CinemaListFrame extends JFrame
 {
-	Button bTheatre1,bTheatre2,bTheatre3,bTheatre4;
+	JLabel lTheatre1,lTheatre2,lTheatre3,lTheatre4;
+	JButton bTheatre1,bTheatre2,bTheatre3,bTheatre4;
 	MyActionListener ml = new MyActionListener(this);
 	
 	CinemaListFrame()
 	{
         super("Theatre");
 		setLayout(null);
-		setBackground(Color.PINK);
-		setBounds(0,0,1200,800);
+		setBounds(0,0,1200,900);
 		
-		bTheatre1 = new Button("R World INOX");
-		bTheatre2 = new Button("Cosmoplex");
-		bTheatre3 = new Button("INOX");
-		bTheatre4 = new Button("Galaxy Cinema");
+		ImageIcon ITheatre1 = new ImageIcon("R_World.png");
+		ImageIcon ITheatre2 = new ImageIcon("cosmoplex.png");
+		ImageIcon ITheatre3 = new ImageIcon("Rajshree.png");
+		ImageIcon ITheatre4 = new ImageIcon("galaxy_cinema.png");
 		
-		bTheatre1.setBounds(150,250,300,80);
-		bTheatre2.setBounds(650,250,300,80);
-		bTheatre3.setBounds(150,600,300,80);
-		bTheatre4.setBounds(650,600,300,80);
+		lTheatre1 = new JLabel();
+		lTheatre1.setIcon(ITheatre1);
+		lTheatre1.setBounds(95,40,370,240);
+		lTheatre1.setVisible(true);
+		
+		lTheatre2 = new JLabel();
+		lTheatre2.setIcon(ITheatre2);
+		lTheatre2.setBounds(630,40,370,240);
+		lTheatre2.setVisible(true);
+		
+		lTheatre3 = new JLabel();
+		lTheatre3.setIcon(ITheatre3);
+		lTheatre3.setBounds(90,450,500,240);
+		lTheatre3.setVisible(true);
+		
+		lTheatre4 = new JLabel();
+		lTheatre4.setIcon(ITheatre4);
+		lTheatre4.setBounds(630,450,500,240);
+		lTheatre4.setVisible(true);
+		
+		bTheatre1 = new JButton("R World INOX");
+		bTheatre2 = new JButton("Cosmoplex");
+		bTheatre3 = new JButton("Rajshree");
+		bTheatre4 = new JButton("Galaxy Cinema");
+		
+		bTheatre1.setBounds(145,290,300,70);
+		bTheatre2.setBounds(670,290,300,70);
+		bTheatre3.setBounds(150,700,300,70);
+		bTheatre4.setBounds(650,700,360,70);
 		
 		Color customColor = new Color(36,118,135);
 		Color customColor1 = new Color(220,118,135);
-		setBackground(customColor);
+		getContentPane().setBackground(customColor);
 		setForeground(Color.white);
 		bTheatre1.setBackground(customColor1);
 		bTheatre2.setBackground(customColor1);
@@ -49,6 +76,10 @@ public class CinemaListFrame extends Frame
 		add(bTheatre2);
 		add(bTheatre3);
 		add(bTheatre4);
+		add(lTheatre1);
+		add(lTheatre2);
+		add(lTheatre3);
+		add(lTheatre4);
 		
         addWindowListener(new WindowAdapter()
 			{
